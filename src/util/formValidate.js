405c8b48cfaaf95bdc444e0036c7c6f27a335b30
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-03-29 00:07:16 +0500

    Create formValidate.js

---
+++ src/util/formValidate.js
@@ -0,0 +1,51 @@
+import * as Yup from "yup";
+
+/**
+ * Password regex, it must contain the following:
+ * 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number.
+ */
+const passwordRequirementRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+export const commonYupValidation = {
+  email: Yup.string()
+    .email("Please enter a vaild email")
+    .required("Email is required"),
+  firstName: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("First name is required"),
+  lastName: Yup.string()
+    .max(20, "Must be 20 characters or less")
+    .required("Last name is required"),
+  role: (roles: string[]) =>
+    Yup.string()
+      .required("Role is required")
+      .test(
+        "is-role",
+        "You need to type or select one of the ones below",
+        (role) => (role ? roles.includes(role) : false)
+      ),
+  genre: (genres: string[]) =>
+    Yup.string().test(
+      "is-genre",
+      "You need to type or select one of the ones below",
+      (genre) => (genre ? genres.includes(genre) : false)
+    ),
+  nickname: Yup.string()
+    .required("Stage name is required")
+    .matches(/^[aA-zZ\s]+$/, "Please only use letters"),
+  password: Yup.string().required("Password is required"),
+  newPassword: Yup.string()
+    .test(
+      "is-trimmed",
+      "Password must not start or end with a space character",
+      (password) => (password ? password === password.trim() : true)
+    )
+    .matches(
+      passwordRequirementRegex,
+      "Minimum 8 characters, at least one uppercase letter, one lowercase letter and one number"
+    ),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("newPassword")],
+    "Passwords must match"
+  ),
+};
